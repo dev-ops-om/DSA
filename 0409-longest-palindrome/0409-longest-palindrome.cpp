95d5df1777c03b1c8ab25158c463526b3049a3a1
Om Shirbhate <shirbhateom2005@gmail.com>
2026-07-26 13:11:05 +0530

    Time: 0 ms (100%), Space: 9.2 MB (86.46%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_set<char>st;
-        int result=0;
-        for(char &ch:s){
-   if(st.count(ch)){
-    result+=2;
-    st.erase(ch);
-   }else{
-    st.insert(ch);
-   }
+        int n=s.length();
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        for(auto &ch:s)
+        {
+            if(ch>='a'){
+                lower[ch-'a']++;
+            }else
+            {
+                upper[ch-'A']++;
+            }
         }
-        if(!st.empty())
-        result++;
+        int count=0;
+        bool oddFlag=0;
+        for(int i=0;i<26;i++){
+            if(lower[i]%2==0)
+            count+=lower[i];
+            else{
+                count+=lower[i]-1;
+                oddFlag=1;
+            }
 
-        return result;
+            if(upper[i]%2==0)
+            count+=upper[i];
+            else{
+                count+=upper[i]-1;
+                oddFlag=1;
+            }
+        }
+        if(oddFlag)
+        count++;
+
+        return count;
     }
 };
