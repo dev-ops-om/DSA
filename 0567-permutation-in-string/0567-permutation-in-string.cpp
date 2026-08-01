c46134dd97881e1475c9533c371cd554f7f3407c
Om Shirbhate <shirbhateom2005@gmail.com>
2026-08-01 17:27:52 +0530

    Time: 0 ms (100%), Space: 10.1 MB (30.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        
-        if (n1 > n2) return false;
-        
-        vector<int> freq_1(26, 0);
-        vector<int> freq_2(26, 0);
+        int n1=s1.size();
+        int n2=s2.size();
 
-        // Build frequency for s1
-        for (char ch : s1) {
-            freq_1[ch - 'a']++;
-        }
+        if(n1>n2)
+        return false;
+ 
+ vector<int>freq_1(26,0);
+  vector<int>freq_2(26,0);
 
-        int i = 0, j = 0;
-        while (j < n2) {
-            freq_2[s2[j] - 'a']++;
+        for(char &ch:s1)
+        freq_1[ch-'a']++;
 
-            // If window size exceeds n1, shrink from left
-            if ((j - i + 1) > n1) {
-                freq_2[s2[i] - 'a']--;
-                i++;
+        int i=0;
+        int j=0;
+
+        while(j<n2){
+
+            freq_2[s2[j]-'a']++;
+
+            if((j-i+1)>n1){
+                 freq_2[s2[i]-'a']--;
+                 i++;
             }
 
-            // If window size matches and frequencies equal → found permutation
-            if ((j - i + 1) == n1 && freq_1 == freq_2) {
-                return true;
-            }
+            if((j-i+1)==n1 && freq_1==freq_2)
+            return true;
 
             j++;
         }
+
         return false;
     }
 };
