c2de5fe4c248fff63a69de8add748f6904773a23
Om Shirbhate <shirbhateom2005@gmail.com>
2026-08-18 18:58:53 +0530

    Time: 3 ms (86.14%), Space: 10.2 MB (99.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> freq(128, 0); // Stores frequencies inside current window
-        int left = 0;
-        int maxlength = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s[i]]++; // Add s[i] to the window
-
-            // If s[i] is a duplicate, shrink window step-by-step from the left
-            while (freq[s[i]] > 1) {
-                freq[s[left]]--;
-                left++;
+        vector<bool>count(256,0);
+        int first=0;
+        int second=0;
+        int len=0;
+        while(second<s.size()){
+            while(count[s[second]]){
+                count[s[first]]=0;
+                first++;
             }
-
-            // At this point, the window s[left...i] is guaranteed to be valid
-            maxlength = max(maxlength, i - left + 1);
+            count[s[second]]=1;
+            len=max(len,second-first+1);
+            second++;
         }
-
-        return maxlength;
+        return len;
     }
 };
