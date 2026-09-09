6ab9f7fb627785833843dc5b61419ba08c237528
Om Shirbhate <shirbhateom2005@gmail.com>
2026-09-09 19:18:35 +0530

    Time: 3 ms (72.18%), Space: 15.7 MB (60.53%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        string rev=s;
-        reverse(rev.begin(),rev.end());
-        string combined=s+'$'+rev;
-        int n=combined.size();
+        if (s.empty()) return "";
 
-        vector<int>lps(n,0);
-        int first=0;
-        int second=1;
-        while(second<n){
-            if(combined[first]==combined[second]){
-                lps[second]=++first;
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        string combined = s + '$' + rev;
+        int n = combined.size();
+
+        vector<int> lps(n, 0);
+        int first = 0;
+        int second = 1;
+
+        while (second < n) {
+            if (combined[first] == combined[second]) {
+                first++;
+                lps[second] = first;
                 second++;
-            }
-            else {
-                if(first!=0){
-                    first=lps[first-1];
-
-                }
-                else{
-                    lps[second]=0;
+            } else {
+                if (first != 0) {
+                    first = lps[first - 1];
+                } else {
+                    lps[second] = 0;
                     second++;
                 }
             }
         }
-        int temp=s.size()-lps[n-1];
-        int start=lps[n-1];
-        string subStr=s.substr(start,s.size());
 
-     reverse(subStr.begin(),subStr.end());
+        // lps[n - 1] is the length of the longest palindromic prefix
+        int pal_len = lps[n - 1];
+        
+        // Take remaining suffix that needs to be mirrored
+        string subStr = s.substr(pal_len);
+        reverse(subStr.begin(), subStr.end());
 
-        return subStr+s;
-
+        return subStr + s;
     }
 };
