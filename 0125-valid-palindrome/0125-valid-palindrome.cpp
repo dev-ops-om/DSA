0c7260f24d1bb492056d1de40b3cde274221a003
Om Shirbhate <shirbhateom2005@gmail.com>
2026-09-13 23:44:04 +0530

    Time: 2 ms (35.08%), Space: 10 MB (52.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,32 @@
 class Solution {
+
+    private:
+    bool isalphaNum(char ch){
+        if((tolower(ch)>='a' && tolower(ch)<='z' )||(ch>='0' && ch<='9')){
+            return true;
+        }
+        return false;
+    }
 public:
     bool isPalindrome(string s) {
-        int left=0;
-        int right=s.length()-1;
-transform(s.begin(), s.end(), s.begin(), ::tolower);
-
-        while(left<right){
-          while(left<right && !isalnum(s[left]))
-          left++;
-         while(left<right && !isalnum(s[right]))
-         right--;
-
-            if(s[left]!=s[right])
+      int left=0;
+      int right=s.size()-1;
+      while(left<right){
+            if(!isalphaNum(tolower(s[left]))){
+                left++;
+                continue;
+            }
+            if(!isalphaNum(tolower(s[right]))){
+                right--;
+                continue;
+            }
+        if(tolower(s[left])!=tolower(s[right])){
             return false;
-
-            left++;
-            right--;
-
         }
-        return true;
+        left++;
+        right--;
+       
+      }
+      return true;
     }
 };
