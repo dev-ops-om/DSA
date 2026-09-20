df05ec362227caa5199b641cc9fcbbded77c6065
Om Shirbhate <shirbhateom2005@gmail.com>
2026-09-21 00:25:14 +0530

    Time: 3 ms (98.88%), Space: 42.7 MB (99.3%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,35 +1,51 @@
 class Solution {
-
-    int helper(vector<int>& nums, int k){
-        int left=0;
-        int n=nums.size();
-        int ans=0;
-        int count=0;
-      vector<int>freq(n+1,0);
-     
-        for(int right=0;right<n;right++){
-freq[nums[right]]++;
-
-     if(freq[nums[right]]==1){
-    count++;
-          } 
-
-        while(count>k){
-            freq[nums[left]]--;
-            if (freq[nums[left]] == 0) {
-       count--;
-    }
-              left++;
-        }
-      
-        ans+=right-left+1;
-        }
-        return ans;
-    }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int x=helper(nums,k);
-        int y=helper(nums,k-1);
-         return x-y;
+        int n=nums.size();
+        vector<int>freq(n+1,0);
+        int count=0;
+        int total=0;
+        int left=0;
+        for(int right=0;right<n;right++){
+            freq[nums[right]]++;
+
+          if(freq[nums[right]]==1){
+    count++;
+          }  
+
+          while(count==k){
+            total+=(n-right);
+            freq[nums[left]]--;
+
+            if(freq[nums[left]]==0){
+                count--;
+            }
+            left++;
+          }
+        }
+
+         freq.assign(n + 1, 0);
+         count=0;
+        
+        left=0;
+        for(int right=0;right<n;right++){
+            freq[nums[right]]++;
+
+          if(freq[nums[right]]==1){
+    count++;
+          }  
+
+          while(count==k+1){
+            total-=(n-right);
+            freq[nums[left]]--;
+
+            if(freq[nums[left]]==0){
+                count--;
+            }
+            left++;
+          }
+        }
+
+return total;
     }
 };
